a4e73064ead1319259fdb0eba837b1571d3f9b53
jrbardin <jess.bardin@elicitinsights.com>
2015-03-18 15:24:21 -0500

    final version for course project submission

--- DevelopingDataProducts/course_project/server.R
+++ DevelopingDataProducts/course_project/server.R
@@ -1,22 +1,37 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# This is the server logic for the Car Finder Shiny web app
 
 library(shiny)
 library(datasets)
 data(mtcars)
+# drop some of the columns
+mtcars <- mtcars[c(1,2,4,7,9)]
 mtcars2 <- cbind(row.names(mtcars), mtcars)
-colnames(mtcars2)[1] <- "Make & Model"
+names(mtcars2)[1] <- "make_model"
 
 shinyServer(function(input, output) {
-
+  
   output$best_cars <- renderDataTable({
-    # filter based on user selections
-    my_cars <- mtcars2[mtcars2$mpg >= input$min_mpg & mtcars2$hp >= input$min_hp, ]
-  }, options = list(orderClasses = TRUE, pageLength = 25),
+    # only refresh table when user hits button
+    input$goButton
+    
+    # don't show anything until user hits button the first time
+    if (input$goButton == 0) {
+      return(NULL)
+    }
+    
+    isolate({
+      # filter based on user selections
+      my_cars <- 
+        mtcars2[mtcars2$mpg >= input$min_mpg 
+                & mtcars2$hp >= input$min_hp
+                & (mtcars2$am == input$trans | input$trans==2), ]
+      # customize names for table header
+      names(my_cars) <- c("Make & Model", "Miles/Gallon", "Cylinders", "Horse Power", "1/4 Mile Time (sec)", "Transmission 0=Automatic 1=Manual")       
+      # return value
+      my_cars
+    })
+  }, options = list(orderClasses = TRUE, pageLength = 10),
   )
 
 })
--- DevelopingDataProducts/course_project/ui.R
+++ DevelopingDataProducts/course_project/ui.R
@@ -1,9 +1,5 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# This is the user-interface definition for the Car Finder Shiny web App
 
 library(shiny)
 library(datasets)
@@ -17,22 +13,34 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
+      h4("Select your criteria below to find the right car for you."),
+      h6("(Don't worry about how much it costs!)"),
+      
       sliderInput("min_mpg",
-                  "Has to get at least this MPG:",
+                  "Minimum Gas Mileage",
                   min = min(mtcars$mpg),
                   max = max(mtcars$mpg),
                   value = min(mtcars$mpg),
                   step = 0.1),
       sliderInput("min_hp",
-                  "And at least this much horse power:",
+                  "Minimum Horse Power",
                   min = min(mtcars$hp),
                   max = max(mtcars$hp),
                   value = min(mtcars$hp),
-                  step = 10)
+                  step = 10),
+      radioButtons("trans", 
+                   choices = list("Automatic" = 0, "Manual" = 1, "Either" = 2), 
+                   label = "Transmission", 
+                   selected = 2),
+      actionButton("goButton", " Find! ", icon = icon("car")),
+      br(),
+      br(),
+      h6(em("Note: results only refresh when you click 'Find'"))
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
+      textOutput("test"),
       dataTableOutput("best_cars")
     )
   )
